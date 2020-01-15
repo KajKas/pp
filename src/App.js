8d855441866a49bf6c6bccd26ec1c6ee2d13fc9d
kajetan <kajetan.kasperuk@neoteric.eu>
2020-01-15 14:30:51 +0100

    fetch articles and display one for testing purposes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,36 @@ import ArticleBig from "./components/ArticleBig/ArticleBig";
 import ArticleSmall from "./components/ArticleSmall/ArticleSmall";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  const url = 'https://newsapi.org/v2/top-headlines?country=pl&apiKey=2f8f24b1943846078e4b9077a993d3d8';
+
+  useEffect(() => {
+     const fetchData = async () => {
+         const result = await axios(url);
+         setArticles(result.data.articles);
+         setIsLoading(false)
+     };
+     fetchData();
+  }, []);
+
+  const getArticles = () => {
+    axios.get(url).then(res => {
+        const articles = res.data;
+        setArticles(articles)
+    })
+  };
+
+  return (
+    <div className="App">
+        <h1>Artykuły</h1>
+        {
+            isLoading ? <p>loading...</p> : <ArticleBig articles={articles[0]}/>
+        }
+        <button onClick={() => getArticles()}>Zobacz więcej</button>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ArticleBig/ArticleBig.js
+++ src/components/ArticleBig/ArticleBig.js
@@ -2,7 +2,22 @@ import React from 'react';
 import './ArticleBig.scss';
 
 function ArticleBig({articles}) {
+    const {urlToImage, author, title} = articles;
 
+    return (
+        <div>
+            <a href="#">
+                {
+                    articles ? <div>
+                        <img src={urlToImage} alt="zdjecie"/>
+                        <h3>{author}</h3>
+                        <h2>{title}</h2>
+                    </div> : 'loading...'
+                }
+
+            </a>
+        </div>
+    );
 }
 
 export default ArticleBig;
