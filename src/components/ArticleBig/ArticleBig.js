45e73fa7433f211fb36c5216b956558e6e9263e9
kajetan <kajetan.kasperuk@neoteric.eu>
2020-01-15 14:16:45 +0100

    create components, install axios and sass

---
+++ src/components/ArticleBig/ArticleBig.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ArticleBig.scss';
+
+function ArticleBig({articles}) {
+
+}
+
+export default ArticleBig;
---
+++ src/components/ArticleSmall/ArticleSmall.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ArticleSmall.scss';
+
+function ArticleSmall() {
+
+}
+
+export default ArticleSmall;
